03de9eb3da309ed005ad752948bd4c60343e13c7
Sam Gleske <sam.mxracer@gmail.com>
2017-04-26 21:56:32 -0700

    Update count active jobs

--- count-active-jobs.groovy
+++ count-active-jobs.groovy
@@ -21,16 +21,23 @@
 /*
    This script counts up all of the active builds across all agents and prints the total.
 */
+import jenkins.model.Jenkins
+
+Jenkins j = Jenkins.instance
+
 int active_builds = 0
-hudson.model.Hudson.instance.slaves.each { slave ->
-    if(!slave.getComputer().isOffline()) {
-        def computer = slave.getComputer()
-        def executors = computer.getExecutors()
+int inactive_executors = 0
+j.slaves.each { slave ->
+    if(!slave.computer.isOffline()) {
+        def executors = slave.computer.executors
         executors.each { executor ->
             if(executor.isBusy()) {
                 active_builds++
             }
+            else {
+                inactive_executors++
+            }
         }
     }
 }
-println "Number of active builds: ${active_builds}"
+println "Queue: ${j.queue.items.size()}, Active: ${active_builds}, Free executors: ${inactive_executors}"
