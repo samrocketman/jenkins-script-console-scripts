5131ec25bafc8afeb0436c57bd279a417443e0ca
Sam Gleske <sam.mxracer@gmail.com>
2017-05-26 20:15:56 -0700

    Support killing a project which may have more than
    one job associated with it.  Typically, a project which has multiple jobs
    should start with a similar name.

---
+++ kill-all-builds-for-project.groovy
@@ -0,0 +1,70 @@
+/*
+    Copyright (c) 2015-2017 Sam Gleske - https://github.com/samrocketman/jenkins-script-console-scripts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*
+   This script will kill all in-progress builds associated with a project that
+   may have one or more jobs.
+*/
+
+import hudson.model.FreeStyleBuild
+import hudson.model.Job
+import hudson.model.Result
+import hudson.model.Run
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.workflow.job.WorkflowRun
+
+if(!binding.hasVariable('dryRun')) {
+    dryRun = true
+}
+if(!binding.hasVariable('projectFullNameStartsWith')) {
+    projectFullNameStartsWith = 'folder/project'
+}
+
+Jenkins.instance.getAllItems(Job.class).findAll {
+    it.fullName.startsWith(projectFullNameStartsWith) && it.isBuilding()
+}.collect { Job job ->
+    job.getBuilds().findAll { Run run ->
+        run.isBuilding()
+    }
+}.each { List listOfRuns ->
+    if(listOfRuns) {
+        listOfRuns.each { Run item ->
+            if(item instanceof WorkflowRun) {
+                WorkflowRun run = (WorkflowRun) item
+                if(!dryRun) {
+                    run.doKill()
+                }
+                println "Killed ${run}"
+            } else if(item instanceof FreeStyleBuild) {
+                FreeStyleBuild run = (FreeStyleBuild) item
+                if(!dryRun) {
+                    run.executor.interrupt(Result.ABORTED)
+                }
+                println "Killed ${run}"
+            } else {
+                println "WARNING: Don't know how to handle ${item.class}"
+            }
+        }
+    }
+}
+null
+
+//null so no result shows up
+null
