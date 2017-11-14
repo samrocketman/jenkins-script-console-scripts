d9a1ff51569bc6e6c8a753276119eb83104b19d6
Sam Gleske <sam.mxracer@gmail.com>
2017-11-13 23:43:32 -0800

    Update code style for list all root projects

--- list-all-root-projects-by-view.groovy
+++ list-all-root-projects-by-view.groovy
@@ -6,22 +6,22 @@ import jenkins.model.Jenkins
 import org.jenkinsci.plugins.multiplescms.MultiSCM
 
 void printSCM(Job j, SCM scm) {
-  switch(scm) {
-    case NullSCM:
-      println "    ${j.displayName} has no SCM configured."
-      break
-    case GitSCM:
-      println "    ${j.displayName} -> ${scm.repositories[0].URIs[0]}"
-      break
-    case MultiSCM:
-      scm.configuredSCMs.each { s ->
-        printSCM(j, s)
-      }
-      break
-    default:
-      throw new Exception("SCM class not supported: ${scm.class}")
-      break
-  }
+    switch(scm) {
+        case NullSCM:
+            println "    ${j.displayName} has no SCM configured."
+            break
+        case GitSCM:
+            println "    ${j.displayName} -> ${scm.repositories[0].URIs[0]}"
+            break
+        case MultiSCM:
+            scm.configuredSCMs.each { s ->
+                printSCM(j, s)
+            }
+            break
+        default:
+            throw new Exception("SCM class not supported: ${scm.class}")
+            break
+    }
 }
 
 j = Jenkins.instance
@@ -29,18 +29,18 @@ j = Jenkins.instance
 discovered_jobs = [].toSet()
 
 Jenkins.instance.views.findAll { v ->
-  v.displayName != j.primaryView.displayName
+    v.displayName != j.primaryView.displayName
 }.each { v ->
-  println v.displayName
-  v.items.each { j ->
-    discovered_jobs << j
-    printSCM(j, j.scm)
-  }
+    println v.displayName
+    v.items.each { j ->
+        discovered_jobs << j
+        printSCM(j, j.scm)
+    }
 }
 
 println "Jobs under primary view '${j.primaryView.displayName}' not covered by other views"
 ((j.primaryView.items as Set) - discovered_jobs).each { j ->
-  printSCM(j, j.scm)
+    printSCM(j, j.scm)
 }
 
 null
