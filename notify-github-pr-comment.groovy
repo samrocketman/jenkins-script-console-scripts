ee4bd65ad0ba645e20e13ae3fe66d6c6c3845cb2
Sam Gleske <sam.mxracer@gmail.com>
2018-05-05 13:27:01 -0700

    Support finding GitHub comments to update by expression

--- notify-github-pr-comment.groovy
+++ notify-github-pr-comment.groovy
@@ -44,13 +44,12 @@ Secret getStringCredential(String id) {
 
 Integer findCommentIdByUser(String credential, String repository, String pr_number, String username, String expression = '') {
 	Secret gh_token = getStringCredential(credential)
-    if(expression) {
-        Pattern.compile(getBranchRegexString()).matcher(branch).matches()
-    }
 	String api_endpoint = "https://api.github.com/repos/${repository}/issues/${pr_number}/comments"
     Reader reader = new URL(api_endpoint).newReader(requestProperties: ['Authorization': "token ${gh_token}".toString(), 'Accept': 'application/vnd.github.v3+json'])
     new JsonSlurper().parse(reader).find {
-        it?.user?.login == username
+        it?.user?.login == username && (
+            !expression || Pattern.compile(expression, Pattern.MULTILINE | Pattern.DOTALL).matcher(it?.body).matches()
+        )
     }?.id ?: 0
 }
 
@@ -60,13 +59,13 @@ void postGHPRComment(String credential, String repository, String pr_number, Str
 	Map data = [
 		body: message
 	]
-	String api_endpoint = "https://api.github.com"
+	String api_endpoint = "https://api.github.com/repos/${repository}/issues"
     if(comment_id) {
-        api_endpoint += "/repos/${repository}/issues/comments/${comment_id}"
+        api_endpoint += "/comments/${comment_id}"
 	    method = 'PATCH'
 	}
 	else {
-	    api_endpoint += "/repos/${repository}/issues/${pr_number}/comments"
+	    api_endpoint += "/${pr_number}/comments"
     }
 	String result = new URL(api_endpoint).openConnection().with {
 		doOutput = true
@@ -81,6 +80,11 @@ void postGHPRComment(String credential, String repository, String pr_number, Str
     //nothing to do with result
 }
 
-String message = 'ODoyle rules'
-int id = findCommentIdByUser('github-token', 'samrocketman/jervis-example-project', '6', 'samrocketman')
+String message = '''
+# heading 1
+
+This is a new kind of comment.
+'''.trim()
+String pattern = '^# heading 1.*'
+int id = findCommentIdByUser('github-token', 'samrocketman/jervis-example-project', '6', 'samrocketman', pattern)
 postGHPRComment('github-token', 'samrocketman/jervis-example-project', '6', message, id)
