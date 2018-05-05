429a627a18586451c8508bb4d1ac2456f589a423
Sam Gleske <sam.mxracer@gmail.com>
2018-05-05 13:36:32 -0700

    Find freestyle jobs which are using specific commands

---
+++ find-all-freestyle-jobs-using-shell-command.groovy
@@ -0,0 +1,39 @@
+/*
+    Copyright (c) 2015-2018 Sam Gleske - https://github.com/samrocketman/jenkins-script-console-scripts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/**
+  Search all freestyle jobs in a Jenkins instance.  Find any job containing any
+  one of the commands being used in a list of strings.
+ */
+import hudson.model.FreeStyleProject
+import hudson.tasks.Shell
+import jenkins.model.Jenkins
+
+//find any of the following strings existing in a freestyle job in Jenkins
+List<String> strings = ['git clone']
+
+println Jenkins.instance.getAllItems(FreeStyleProject.class).findAll { job ->
+  job.builders.findAll { it in Shell } &&
+  true in job.builders.findAll { it in Shell }.collect { shell ->
+      strings.findAll { shell.command.contains(it) } as Boolean
+  }
+}.collect { job ->
+    job.absoluteUrl
+}.join('\n')
