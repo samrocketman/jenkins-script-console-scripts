980b718a552f87c14f0cd0f2ebe3bfee0610d911
marco2704 <marcodavalosmantilla@gmail.com>
2018-08-17 20:07:30 +0200

    Add license to cleanup-all-builds.groovy

--- cleanup-all-builds.groovy
+++ cleanup-all-builds.groovy
@@ -1,12 +1,36 @@
-/*** BEGIN META {
-  "name" : "Builds Cleaner",
-  "comment" : "Delete all builds keeping ${MAX_BUILDS} from all job of the Jenkins Instance. It checks build is not building and it also keeps the keep forever, lastStableBuild, lastSuccessfulBuild, lastSuccessfulBuild, lastUnstableBuild, lastUnsuccessfulBuild in case are not included in the ${MAX_BUILDS}.",
-  "parameters" : [],
-  "core": "2.107.3",
-  "authors" : [
-    { name : "Marco Davalos" }
-  ]
-} END META**/
+/*
+    Copyright (c) 2018 Marco Davalos https://github.com/samrocketman/jenkins-script-console-scripts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+    This script deletes all builds from all job of the Jenkins instance keeping the 
+    last ${MAX_BUILDS} builds. It checks the build to delete is not building at that
+    moment and it also keeps the builds that are:
+
+        - keepLog (marked as "Keep this build forever")
+        - lastStableBuild
+        - lastSuccessfulBuild
+        - lastSuccessfulBuild
+        - lastUnstableBuild
+        - lastUnsuccessfulBuild
+*/
 
 import jenkins.model.Jenkins
 import hudson.model.Job
