29b5957626a9af40e62787af9e1bcfbc44ed1994
Sam Gleske <sam.mxracer@gmail.com>
2019-11-07 22:23:11 -0500

    Reset stale lockable resources script

---
+++ reset-stale-lockable-resources.groovy
@@ -0,0 +1,35 @@
+/*
+    Copyright (c) 2015-2019 Sam Gleske - https://github.com/samrocketman/jenkins-script-console-scripts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*
+   Sometimes builds do not release locks via the pipeline lock step.  This
+   script iterates all locks and forces them to be released if a build has a
+   lock but is no longer running.
+ */
+import jenkins.model.Jenkins
+
+def lockManager = Jenkins.instance.getExtensionList('org.jenkins.plugins.lockableresources.LockableResourcesManager')[0]
+
+lockManager.resources.findAll { lock ->
+    lock.isLocked() && !lock.build.isBuilding()
+}.each { lock ->
+    println "Resetting stale lock ${lock}"
+    lock.reset()
+}
