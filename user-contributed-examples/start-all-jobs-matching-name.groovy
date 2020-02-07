e01c1238c25206b147d716ff6416a8089beb6ebb
Sam Gleske <sam.mxracer@gmail.com>
2020-02-07 02:56:55 -0500

    Adding comment header and code changes
    cc @deepy

--- user-contributed-examples/start-all-jobs-matching-name.groovy
+++ user-contributed-examples/start-all-jobs-matching-name.groovy
@@ -1,5 +1,15 @@
-  Jenkins.instance.getAllItems(AbstractItem.class).findAll {
-    if (it.name.endsWith('-cleanup')) {
-      return it.name
-    }
-  }.each { it.scheduleBuild(0, new Cause.UserIdCause()) }
+/**
+    Author: https://github.com/deepy
+    Warning: This code is unlicensed.
+    See https://github.com/samrocketman/jenkins-script-console-scripts/blob/master/user-contributed-examples/README.md
+  */
+
+import hudson.model.Cause
+import hudson.model.Job
+import jenkins.model.Jenkins
+
+Jenkins.instance.getAllItems(AbstractItem.class).findAll {
+    it.name.endsWith('-cleanup')
+}.each {
+    it.scheduleBuild(0, new Cause.UserIdCause())
+}
