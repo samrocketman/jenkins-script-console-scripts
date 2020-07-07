d1924d8d4c184ba9089b9d0562560b2cd9e9330b
Sam Gleske <sam.mxracer@gmail.com>
2020-07-06 23:05:03 -0400

    Script to find running pipeline jobs

---
+++ find-running-pipeline-jobs.groovy
@@ -0,0 +1,34 @@
+/*
+    Copyright (c) 2015-2020 Sam Gleske - https://github.com/samrocketman/jenkins-script-console-scripts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*
+   This script searches the Jenkins server runtime and finds all jobs which are
+   running.  Even those which do not plainly show up in the executor UI.
+ */
+
+import jenkins.model.Jenkins
+
+String findRunningPipelineJobs() {
+    Jenkins.instance.getComputer('').getOneOffExecutors().collect {
+        "${Jenkins.instance.rootUrl}${it.currentExecutable.url}"
+    }.join('\n')
+}
+
+println findRunningPipelineJobs()
